1dd80c323270ad8ac304a0cf143afee35d42a4bd
Galahad <131298570+Gal4h4d@users.noreply.github.com>
2025-06-16 15:57:11 -0500

    Fix: Palabras con errores

--- corte 3/clases/Ejercicios/Talleres/Uso de Documentos anidado No SQL/Ejercicio de consulta sin Where.js
+++ corte 3/clases/Ejercicios/Talleres/Uso de Documentos anidado No SQL/Ejercicio de consulta sin Where.js
@@ -7,19 +7,6 @@ db.pokemons.find(
   }
 )
 
-•	Para mas elementos game:
-db.pokemons.find(
-  {
-    $expr: {
-      $gte: [{ $size: "$games" }, 5]
-    }
-  },
-  {
-    nombre: true,
-    games: true
-  }
-)
-
 •  $expr: permite usar expresiones de agregación en consultas estándar.
 •  $size: obtiene el tamaño del arreglo.
 •  $gte: compara si el tamaño es mayor o igual a 5.
