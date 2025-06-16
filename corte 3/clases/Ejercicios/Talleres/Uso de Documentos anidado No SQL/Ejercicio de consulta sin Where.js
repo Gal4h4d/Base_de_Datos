e9025fe69a89e011485d584133cbfc1b91222ebb
Galahad <131298570+Gal4h4d@users.noreply.github.com>
2025-06-16 16:01:59 -0500

    Fix: Codigo con errores

--- corte 3/clases/Ejercicios/Talleres/Uso de Documentos anidado No SQL/Ejercicio de consulta sin Where.js
+++ corte 3/clases/Ejercicios/Talleres/Uso de Documentos anidado No SQL/Ejercicio de consulta sin Where.js
@@ -1,11 +1,27 @@
 🔸 9. Consulta para mongo sin  utilizar el “where”
  
-•	Para solamente 5 elementos :
+//•	Para solamente 5 elementos :
 db.pokemons.find(
   {
     games: { $exists: true, $not: { $size: 5 } } 
   }
 )
+
+//Para mas de 5
+db.pokemons.find(
+  {
+    games: { $exists: true, $type: "array" },
+    $expr: {
+      $gte: [{ $size: "$games" }, 5]
+    }
+  },
+  {
+    nombre: 1,
+    games: 1,
+    _id: 0
+  }
+);
+
 
 •  $expr: permite usar expresiones de agregación en consultas estándar.
 •  $size: obtiene el tamaño del arreglo.
